be127082350fb559172621dd933e285f40e79220
abhigyanhedau2 <abhigyanhedau2@gmail.com>
2024-05-20 09:53:34 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; // Import the Inter font
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
